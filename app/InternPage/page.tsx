7dc999f9f485ec820393bdda48c9289cd8754b7d
Alex Diaz <alexdiaz10129@gmail.com>
2024-01-28 22:20:41 -0600

    changing format of intern app.tsx

--- app/InternPage/page.tsx
+++ app/InternPage/page.tsx
@@ -3,7 +3,10 @@ import React from "react"
 const PricingPage = () => {
     return (
         <div>Congratulations on the new internship, Lets start making your experience perfect:
-            <h1>Name: </h1>
+            <h2>Name: </h2>
+            <h2>Company: </h2>
+            <h2>City: </h2>
+            <h2>Kansas: </h2>
         </div>
     )
 }
