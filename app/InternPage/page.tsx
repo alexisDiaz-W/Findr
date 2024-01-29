eed6719cad9e068323172ac0424be02e4a6ae875
Alex Diaz <alexdiaz10129@gmail.com>
2024-01-28 22:10:34 -0600

    fixing intern page

--- app/InternPage/page.tsx
+++ app/InternPage/page.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 
-const InternPage = () => {
+const PricingPage = () => {
     return (
-        <div>Congratulation on your new internship.
-            <h1>Let's get you connected with the other interns in your area.</h1>
+        <div>Pricing: 
+            <h1> $ 10 BILLION</h1>
         </div>
     )
 }
 
-export default InternPage
+export default PricingPage
