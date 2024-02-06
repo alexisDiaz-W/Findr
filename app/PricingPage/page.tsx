091cc921af22b5c66e6f037af773a3d101aed793
Alexis Diaz <alexdiaz10129@gmail.com>
2024-02-05 22:47:38 -0600

    Revert "Updated pricing and intern sign up page"

--- app/PricingPage/page.tsx
+++ app/PricingPage/page.tsx
@@ -107,7 +107,8 @@ export default function Example() {
           <div className="p-8 sm:p-10 lg:flex-auto">
             <h3 className="text-2xl font-bold tracking-tight text-gray-900">Beginner membership</h3>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              Start your interns off with the essentials.
+              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
+              repellendus etur quidem assumenda.
             </p>
             <div className="mt-10 flex items-center gap-x-4">
               <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
@@ -149,7 +150,8 @@ export default function Example() {
           <div className="p-8 sm:p-10 lg:flex-auto">
             <h3 className="text-2xl font-bold tracking-tight text-gray-900">Intermediate membership</h3>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              Meet our intermediate plan. It comes with even more features.
+              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
+              repellendus etur quidem assumenda.
             </p>
             <div className="mt-10 flex items-center gap-x-4">
               <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
@@ -191,7 +193,8 @@ export default function Example() {
           <div className="p-8 sm:p-10 lg:flex-auto">
             <h3 className="text-2xl font-bold tracking-tight text-gray-900">Elite membership</h3>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              Meet our elite plan, our most comprehensive plan to give your interns the perfect experience.
+              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
+              repellendus etur quidem assumenda.
             </p>
             <div className="mt-10 flex items-center gap-x-4">
               <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
