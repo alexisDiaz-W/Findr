0ad2110df2bb1bef4549c0fe8fa78be23d5ce8b2
mannyllamas <manny.m.llamas@gmail.com>
2024-02-17 19:36:18 -0600

    Test Findr logo font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { Jura } from "next/font/google";
 import "./globals.css";
+ 
 
-
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 
 const jura_init = Jura({
@@ -15,20 +15,34 @@ const jura_init = Jura({
  
 });
 
-
-export const metadata: Metadata = {
-  title: "Findr",
-  description: "Giving your interns the perfect experience.",
-};
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  return ( 
-    <html lang="en">
+}) {
+  return (
+    <html lang='en'>
       <body className={jura_init.variable}>{children}</body>
     </html>
-  ); 
+  );
 }
+
+
+
+// export const metadata: Metadata = {
+//   title: "Findr",
+//   description: "Giving your interns the perfect experience.",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return ( 
+//     <html lang="en">
+//       <body className={jura_init.variable}>{children}</body>
+//     </html>
+//   ); 
+// }
+ 
