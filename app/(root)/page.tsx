3ea2b6dbd7c46dd81c669edc1b753378cfcc55d8
mannyllamas <manny.m.llamas@gmail.com>
2024-02-18 16:00:14 -0600

    Removing unused/commented code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,11 @@
-
-
 "use client";
-// import React, { useState, useEffect } from 'react';
-import Image from "next/image";
+
 import Link from "next/link";
-import Head from "next/head"
-//  import React, { useState, useEffect } from "react";
-// import ResponsiveHamburgerMenu from "./Hmenu";
-// import { FiHome } from 'react-icons/fi';
-// import Navbar from "./Navbar";
-import { Alert } from "flowbite-react";
-import { Dropdown } from "flowbite-react";
-import SignInIconButton from "../ResponsiveSignInButton/SingInButton";
 import NavBar from "../NavBar/navBar";
 import React from 'react';
 
-
 export default function Home() {
-  // const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
+
   return (
 
     <main className="text-gray-600  flex min-h-screen flex-col items-center justify-between p-24">
